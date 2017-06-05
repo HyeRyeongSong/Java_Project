5456cb4fa936dcdc8ff4b917024289ca5eb83da6
MinHyeJun <m2j9702@naver.com>
2017-06-05 17:52:00 +0900

    속성Pane에 버튼 추가

--- src/AttributePane.java
+++ src/AttributePane.java
@@ -1,12 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by m2j97 on 2017-06-05.
  */
-public class AttributePane extends JPanel
+public class AttributePane extends JPanel implements ActionListener
 {
     private RectangleEditor2 re;
+
     private JTextField x;
     private JTextField y;
     private JTextField w;
@@ -14,6 +17,8 @@ public class AttributePane extends JPanel
     private JTextField text;
     private JComboBox type;
     private JTextField var;
+
+    private JButton jb;
 
     public AttributePane()
     {
@@ -25,26 +30,32 @@ public class AttributePane extends JPanel
         text = new JTextField();
         type = new JComboBox();
         var = new JTextField();
+        jb = new JButton("적용");
 
-        setLayout(new GridLayout(7, 2, 2, 5));
-        add(new JLabel("시작 x 좌표"), 0);
-        add(x, 1);
-        add(new JLabel("시작 y 좌표"), 2);
-        add(y, 3);
-        add(new JLabel("너비"), 4);
-        add(w, 5);
-        add(new JLabel("높이"), 6);
-        add(h, 7);
-        add(new JLabel("컴포넌트의 텍스트 속성값"), 8);
-        add(text, 9);
-        add(new JLabel("컴포넌트 타입"), 10);
-        add(type, 11);
-        add(new JLabel("컴포넌트 변수명"), 12);
-        add(var, 13);
+        setLayout(new BorderLayout());
+        JPanel editor = new JPanel(new GridLayout(7, 2, 2, 5));
+        add(editor,BorderLayout.CENTER);
+        add(jb, BorderLayout.SOUTH);
+
+        editor.add(new JLabel("시작 x 좌표"), 0);
+        editor.add(x, 1);
+        editor.add(new JLabel("시작 y 좌표"), 2);
+        editor.add(y, 3);
+        editor.add(new JLabel("너비"), 4);
+        editor.add(w, 5);
+        editor.add(new JLabel("높이"), 6);
+        editor.add(h, 7);
+        editor.add(new JLabel("컴포넌트의 텍스트 속성값"), 8);
+        editor.add(text, 9);
+        editor.add(new JLabel("컴포넌트 타입"), 10);
+        editor.add(type, 11);
+        editor.add(new JLabel("컴포넌트 변수명"), 12);
+        editor.add(var, 13);
     }
 
     public void setEditorPane(RectangleEditor2 e)
     {
+        //에디터페인 정보 실시간으로 받기 위해 필드로 저장
         re = e;
     }
 
@@ -56,5 +67,13 @@ public class AttributePane extends JPanel
         h.setText(Integer.toString(e.getH()));
         text.setText("text");
         var.setText("var");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        //변경된 속성 적용
+        re.revalidate();
+        re.repaint();
     }
 }
